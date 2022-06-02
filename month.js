7ccf635d0195a3f17a6b31a9bb1a51d3502b8e71
isamir909 <samirlohiya909@gmail.com>
2022-06-02 18:38:04 +0530

    updated problem on route.js

---
+++ month.js
@@ -0,0 +1,29 @@
+// const { Console } = require("console")
+
+//  let c=[ ]
+// for(let i=1; i<30;i++){
+//    c.push(i)
+   
+// }
+// console.log(c)
+
+let o=[
+    1,  2,  3,  4,  5,  6,  7,  8,  9,
+   10, 11, 12, 13, 14, 15, 16, 17, 18,
+   19, 20, 21, 22, 23, 24, 25, 26, 27,
+   28, 29
+ ]
+ let odd=[]
+ for (var i = 1; i < o.length; i++) {
+    if (o[i] % 2 !=1) {
+       
+        odd.push(i)
+        
+    }
+   
+}
+let req=odd.slice(0,10);
+console.log(req);
+// console.log(o)
+
+
