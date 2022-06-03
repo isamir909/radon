f76ea620aabaf9c89591ae187718c2b316485d47
isamir909 <samirlohiya909@gmail.com>
2022-06-03 09:20:58 +0530

    my first api assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,66 +1,175 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
-
+const { toNumber } = require('lodash');
+// const myHelper = require('../util/helper')
+// const underscore = require('underscore')
+// const movielist=require('./'); 
 const router = express.Router();
-const lodash = require('lodash')
-// router.get('/test-me', function (req, res) {
-//     myHelper.printDate()
-//     myHelper.getCurrentMonth()
-//     myHelper.getCohortData()
-//     let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-//     console.log('The first element received from underscope function is '+firstElement)
-//     res.send('My first ever api!')
-// });
-
-router.get('/hello', function (req, res) {
-    const months = [...Array(12).keys()].map(key => new Date(0, key).toLocaleString('en', { month: 'long' }))
-    // months=['January','February','March','April','May','June','July','August','September','October','November','December']
-    let a = lodash.chunk(months, 2);
-    console.log(a)
+// const lodash = require('lodash');
 
 
-    let odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,]
-    let tail = lodash.tail(odd);
-    console.log(tail)
 
 
-    let array1 = [1, 2, 5, 4, 1, 4, 2]
-    let array2 = [1, 2, 5, 4, 1, 4, 7, 1, 7, 2]
-    let array3 = [1, 2, 5, 4, 1, 4, 2, 5, 8, 5]
-    let array4 = [1, 2, 5, 4, 1, 4, 2, 4, 7]
-    let array5 = [1, 2, 5, 4, 1, 4, 2, 6, 7, 8, 4, 6]
-    let combine = lodash.union(array1, array2, array3, array4, array5)
-    console.log(combine);
+// ===========================problem 1=========================================
+// Create an API for GET /movies that returns a list of movies. 
+// Define an array of movies in your code and return the value in response.
 
+router.get('/GET/movies', function (req, res) {
 
-let myFav=[["horror","The Shining"],["dramas","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+    const array1 = ["Spider-Man: No Way Home", "The Father ", "The Last Duel"]
 
-        let obj=lodash.fromPairs(myFav)
-        console.log(obj)
-
-
-    res.send('Hello there! here is the list of months')
+    res.send(array1)
 });
 
-// router.get('/candidates', function(req, res){
-//     console.log('Query paramters for this request are '+JSON.stringify(req.query))
-//     let gender = req.query.gender
-//     let state = req.query.state
-//     let district = req.query.district
-//     console.log('State is '+state)
-//     console.log('Gender is '+gender)
-//     console.log('District is '+district)
-//     let candidates = ['Akash','Suman']
-//     res.send(candidates)
-// })
 
-// router.get('/candidates/:canidatesName', function(req, res){
-//     console.log('The request objects is '+ JSON.stringify(req.params))
-//     console.log('Candidates name is '+req.params.canidatesName)
-//     res.send('Done')
-// })
+// =======================problem 2,3=============================================
+//                    
+//                   *******case 1*******
+// Create an API GET /movies/:indexNumber 
+// (For example GET /movies/1 is a valid request and it should return the movie in your array at index 1).
+//  You can define an array of movies again in your api.
+
+
+//                      *******case 2*******
+// Handle a scenario in problem 2 where if the index is 
+// greater than the valid maximum value a message is returned that tells the user to 
+// use a valid index in an error message.
+
+
+router.get('/GET/movies/:indexNumber', function (req, res) {
+    const movie = ["Spider-Man: No Way Home", "The Father ", "The Last Duel"]
+    let a = req.params.indexNumber
+    if (a <= movie.length) {
+        let indexNumber = movie[a]
+
+        res.send(indexNumber)
+    }
+    else {
+        res.send("invalid input")
+    }
+
+});
 
 
 module.exports = router;
-// adding this comment for no reason
+
+
+// =======================problem 4,5=================================================
+
+// Write another api called GET /films. Instead of an array of strings define an array of
+//  movie objects this time. Each movie object should have values - id, name. An example of movies array is 
+
+
+
+router.get('/GET/films/:filmId', function (req, res) {
+    let filmList = [{
+        "id": 1,
+        "name": "The Shining"
+    },
+    {
+        "id": 2,
+        "name": "Incendies"
+    },
+    {
+        "id": 3,
+        "name": "Rang de Basanti"
+    },
+    {
+        "id": 4,
+        "name": "Finding Nemo"
+    }]
+    let e = req.params.filmId
+    let f = Number(e) - 1
+    // let g=filmList[f]
+    // console.log(g)
+    if (f < filmList.length) {
+        res.send(filmList[f])
+
+    }
+    else {
+        res.send("No movie exists with this id")
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // router.get('/test-me', function (req, res) {
+// //     myHelper.printDate()
+// //     myHelper.getCurrentMonth()
+// //     myHelper.getCohortData()
+// //     let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
+// //     console.log('The first element received from underscope function is '+firstElement)
+// //     res.send('My first ever api!')
+// // });
+
+// router.get('/hello', function (req, res) {
+//     const months = [...Array(12).keys()].map(key => new Date(0, key).toLocaleString('en', { month: 'long' }))
+//     // months=['January','February','March','April','May','June','July','August','September','October','November','December']
+//     let a = lodash.chunk(months, 2);
+//     console.log(a)
+
+
+//     let odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,]
+//     let tail = lodash.tail(odd);
+//     console.log(tail)
+
+
+//     let array1 = [1, 2, 5, 4, 1, 4, 2]
+//     let array2 = [1, 2, 5, 4, 1, 4, 7, 1, 7, 2]
+//     let array3 = [1, 2, 5, 4, 1, 4, 2, 5, 8, 5]
+//     let array4 = [1, 2, 5, 4, 1, 4, 2, 4, 7]
+//     let array5 = [1, 2, 5, 4, 1, 4, 2, 6, 7, 8, 4, 6]
+//     let combine = lodash.union(array1, array2, array3, array4, array5)
+//     console.log(combine);
+
+
+// let myFav=[["horror","The Shining"],["dramas","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+
+//         let obj=lodash.fromPairs(myFav)
+//         console.log(obj)
+
+
+//     res.send('Hello there! here is the list of months')
+// });
+
+// // router.get('/candidates', function(req, res){
+// //     console.log('Query paramters for this request are '+JSON.stringify(req.query))
+// //     let gender = req.query.gender
+// //     let state = req.query.state
+// //     let district = req.query.district
+// //     console.log('State is '+state)
+// //     console.log('Gender is '+gender)
+// //     console.log('District is '+district)
+// //     let candidates = ['Akash','Suman']
+// //     res.send(candidates)
+// // })
+
+// // router.get('/candidates/:canidatesName', function(req, res){
+// //     console.log('The request objects is '+ JSON.stringify(req.params))
+// //     console.log('Candidates name is '+req.params.canidatesName)
+// //     res.send('Done')
+// // })
+
+
+
+// // adding this comment for no reason
