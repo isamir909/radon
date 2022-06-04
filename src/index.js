017307d264a4a49f6c242dfd63b0ee9de9d1b0c5
isamir909 <samirlohiya909@gmail.com>
2022-06-04 14:21:35 +0530

    adding data from body (getting input)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://samirlohiya909:Lohiya@123@cluster0.bebqxbp.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+// mongoose.connect("mongodb+srv://samirlohiya909:lohiya123@cluster0.ld7kv.mongodb.net/?retryWrites=true&w=majority",{
+//     useNewUrlParser:true
+
+// })
+
+
+// .then( () => console.log("mongoDB is connected"))
+// .catch(err => console.log(err))
+
+
+
+mongoose.connect("mongodb+srv://samirlohiya909:lohiya123@cluster0.ld7kv.mongodb.net/samirlohiya909?retryWrites=true&w=majority",{
+        useNewUrlParser:true
+    
+    })
+    
+    
+.then( () => console.log("mongoDB is connected"))
+.catch(err => console.log(err))
+
+
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,87 @@
+const { request } = require('express');
 const express = require('express');
 const router = express.Router();
+let data = [
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "Jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "Rahul",
+        "dob": "2/2/1995",
+        "gender": "male",
+        "city": "surat",
+        "sports": [
+            "cricket"
+        ]
+    },
+]
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+router.post("/players",function(req,res){
+    // let c=Number(0)
+    let a=req.body.key.value
+    let b=req.body.dob
+    let c=req.body.gender
+    let d=req.body.sports
+    let e=req.body.city
+    console.log(a)
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+// if(a!=data){
+//     data.push(a,b,c,d,e)
+//     res.send(data)
+// }
 
+//      else{
+//     res.send("player is already present", data)
+//  }
+});
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+// router.post("/test-post-3", function(req, res) {
+//     // let id = req.body.user
+//     // let pwd= req.body.password
 
+//     // console.log( id , pwd)
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
+//     console.log( req.body )
 
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
+//     res.send(  { msg: "hi" , status: true }  )
+// })
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
+
+// router.post("/test-post", function(req, res) {
+//     res.send([ 23, 45 , 6])
+// })
+
+
+// router.post("/test-post-2", function(req, res) {
+//     res.send(  { msg: "hi" , status: true }  )
+// })
+
+// router.post("/test-post-3", function(req, res) {
+//     // let id = req.body.user
+//     // let pwd= req.body.password
+
+//     // console.log( id , pwd)
+
+//     console.log( req.body )
+
+//     res.send(  { msg: "hi" , status: true }  )
+// })
+
+
+
+// router.post("/test-post-4", function(req, res) {
+//     let arr= [ 12, "functionup"]
+//     let ele= req.body.element
+//     arr.push(ele)
+//     res.send(  { msg: arr , status: true }  )
+// })
 
 module.exports = router;
