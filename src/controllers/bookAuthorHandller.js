edef74f5bfc68f475308a8fa8c60c63c9b0fb666
isamir909 <samirlohiya909@gmail.com>
2022-06-08 23:44:08 +0530

    completed assignment with 2 new problems

--- src/controllers/bookAuthorHandller.js
+++ src/controllers/bookAuthorHandller.js
@@ -1,53 +1,74 @@
-const authorModel=require('../models/authorModel')
-const booksDataModel=require('../models/bookModel')
+const authorModel = require('../models/authorModel')
+const booksDataModel = require('../models/bookModel')
 
-let createAuthor=async function (req, res) {
+let createAuthor = async function (req, res) {
     let authorData = req.body
     let Data = await authorModel.create(authorData)
     res.send(Data)
 }
 
 
-let createBook=async function (req, res) {
+let createBook = async function (req, res) {
     let bookdata = req.body
     let Data = await booksDataModel.create(bookdata)
     res.send(Data)
 }
 
-let booksByChetanBhagat=async function(req,res){
-    let iD= await authorModel.find({ author_name:"Chetan Bhagat"}).select({"author_id":1,_id:0})
+let booksByChetanBhagat = async function (req, res) {
+    let iD = await authorModel.find({ author_name: "Chetan Bhagat" }).select({ "author_id": 1, _id: 0 })
     // console.log(iD)
-    let bookdata= await booksDataModel.find({author_id:iD[0].author_id})
-    res.send({msg:bookdata})
+    let bookdata = await booksDataModel.find({ author_id: iD[0].author_id })
+    res.send({ msg: bookdata })
 }
 
 
-let updateBookPrice=async function(req,res){
-    let book=await booksDataModel.findOneAndUpdate({name:"Two states"},{$set:{price:100}},{new:true})
+let updateBookPrice = async function (req, res) {
+    let book = await booksDataModel.findOneAndUpdate({ name: "Two states" }, { $set: { price: 100 } }, { new: true })
     // console.log(book)
-    let authorData= await authorModel.find({author_id:book.author_id}).select("author_name")
-    let price=book.price
-    res.send({msg:authorData,price})
+    let authorData = await authorModel.find({ author_id: book.author_id }).select("author_name")
+    let price = book.price
+    res.send({ msg: authorData, price })
 }
 
 
-let priceRange = async function(req,res){
-    let books = await booksDataModel.find({price:{$gte:50,$lte:100}}).select({author_id:1});
-    //  console.log(books)
-    books =books.map((book)=> book.author_id);
-    const authors =await authorModel.find({author_id:{$in: books}});
-    let authorName =authors.map((name)=> name.author_name);
-   res.send(authorName);
-      
-  }
+let priceRange = async function (req, res) {
+    let books = await booksDataModel.find({ price: { $gte: 50, $lte: 100 } }).select({ author_id: 1 });
+    console.log(books)
+    books = books.map((book) => book.author_id);
+    console.log(books)
+    // const authors =await authorModel.find({author_id:{$in: books}});
+    // let authorName =authors.map((name)=> name.author_name);
+    res.send(authorName);
+
+}
 
 
+// Write an api GET /books-by-authorid/<Author_Id> (for example /books/1 or /books/2 etc) that returns all the books written by the author with an id <Author_Id>. Only return the names of these books
+
+let bookBYAuthID = async function (req, res) {
+    let input = req.params.id
+    let books = await booksDataModel.find({ author_id: input },).select({ name: 1, _id: 0 })
+    res.send(books)
+}
 
 
-module.exports.createAuthor=createAuthor
-module.exports.createBook=createBook
-module.exports.booksByChetanBhagat=booksByChetanBhagat
-module.exports.updateBookPrice=updateBookPrice
-module.exports.priceRange=priceRange
+// Find a list of authors whose are older than 50 years of age with at least one book that has a rating greater than 4. Only include the author’s names and their ages in the response for this api
+
+let findByRating = async function (req, res) {
+    let rating = await booksDataModel.find({ rating: { $gt: 4 } }).select({ author_id: 1, _id: 0 })
+    let id = rating.map(function (ele) { return ele.author_id })
+    let name = await authorModel.find({ $and: [{ age: { $gt: 50 } }, { author_id: id }] })
+        .select({ author_name: 1, age: 1, _id: 0 })
+    res.send(name)
+}
+
+
+module.exports.createAuthor = createAuthor
+module.exports.createBook = createBook
+module.exports.booksByChetanBhagat = booksByChetanBhagat
+module.exports.updateBookPrice = updateBookPrice
+module.exports.priceRange = priceRange
+module.exports.bookBYAuthID = bookBYAuthID
+module.exports.findByRating = findByRating
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,19 +3,31 @@ const { route } = require('express/lib/application');
 const { type, redirect } = require('express/lib/response');
 const { default: mongoose } = require('mongoose');
 const router = express.Router();
-const bookAuthorHandller=require('../controllers/bookAuthorHandller')
+const bookAuthorHandller = require('../controllers/bookAuthorHandller')
 
 
 
-router.post('/createAuthor',bookAuthorHandller.createAuthor )
+router.post('/createAuthor', bookAuthorHandller.createAuthor)
 
-router.post('/createBook',bookAuthorHandller.createBook )
+router.post('/createBook', bookAuthorHandller.createBook)
 
 router.post('/booksByChetanBhagat', bookAuthorHandller.booksByChetanBhagat);
 
-router.post('/updateBookPrice',bookAuthorHandller.updateBookPrice)
+router.post('/updateBookPrice', bookAuthorHandller.updateBookPrice)
 
 router.post('/priceRange', bookAuthorHandller.priceRange);
+
+
+
+// Write an api GET /books-by-authorid/<Author_Id> (for example /books/1 or /books/2 etc) that returns all the books written by the author with an id <Author_Id>. Only return the names of these books
+
+
+router.get('/books-by-authorid/:id', bookAuthorHandller.bookBYAuthID);
+
+// Find a list of authors whose are older than 50 years of age with at least one book that has a rating greater than 4. Only include the author’s names and their ages in the response for this api
+
+
+router.get('/findByRating', bookAuthorHandller.findByRating);
 
 
 
@@ -32,8 +44,12 @@ module.exports = router;
 
 
 
-
-
+// router.get('/findByRating', async function(req,res){
+//     let author= await authorModel.find({age:{$gt:50}}).select( {author_id:1,_id:0})
+//     let book = await booksDataModel.findOne({rating:{$gt:4}}).select()
+//     {author_id:author.map(x)(x)}
+//     authID=author.map((name) =>name)
+// })
 
 
 
